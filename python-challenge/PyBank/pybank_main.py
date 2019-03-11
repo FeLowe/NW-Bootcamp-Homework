48d4208651e6af3961ea17ce0e313dbe6817293f
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2019-03-10 19:40:24 -0500

    unable to fix comma separation on output file

--- python-challenge/PyBank/pybank_main.py
+++ python-challenge/PyBank/pybank_main.py
@@ -13,44 +13,44 @@ with open(file) as csvfile:
     header_out = next(csv_data, None)
 
     for row in csv_data:
-        #Track Totals
+        # push each month to months list
         months.append(row[0])
-        #print months
-        
+        # push each value to prof_loss
         prof_loss.append(int(row[1]))
-        #print prof_loss
-
+    #gets len of months, print single count number as it is oulside the loop. 
     total_months = len(months)
-
+    
+    #set variables to start at 0 value
     gt_revenue_increase = prof_loss[0]
     gt_revenue_decrease = prof_loss[0]
     total_prof_loss = 0
-
+    
+    #loop through each row in prof_loss list 
     for row in range(len(prof_loss)):
 
+        #gets greatest revenue increse value and month
         if prof_loss[row] >= gt_revenue_increase:
             gt_revenue_increase = prof_loss[row]
-            print gt_revenue_increase
             gt_revenue_increase_month = months[row]
-            print gt_revenue_increase_month
-
+        #gets greatest revenue descrease value and month
         elif prof_loss[row] < gt_revenue_decrease:
-            gt_revenue_decrease = prof_loss[row]
-            print gt_revenue_decrease        
+            gt_revenue_decrease = prof_loss[row]      
             gt_revenue_decrease_month = months[row]
-            print gt_revenue_decrease_month  
-
-        total_prof_loss += prof_loss [row] 
-    print total_prof_loss
-     # Average Calculation       
+        
+        #get total revenuw
+ total_prof_loss += prof_loss [row] 
+    
+#Average calculation       
 average_change = round(total_prof_loss / total_months,2) 
-print average_change
 
-# Specify the file to write to
-output_path = os.path.join("pybank_output.txt")
+#File to write results to
+utput_path = os.path.join("pybank_output.txt")
+output_path = os.path.join("pybank_output.csv")
+
 
 # Open the file using "write" mode. Specify the variable to hold the contents
-with open(output_path, 'w') as csvfile:
+with open(output_path,'w') as csvfile:
+
 
     # Initialize csv.writer
     csvwriter = csv.writer(csvfile)
@@ -58,12 +58,12 @@ with open(output_path, 'w') as csvfile:
     # Write the first row (column headers)
     csvwriter.writerow('Financial Analysis\n')
     csvwriter.writerow('----------------------------' + '\n')
-    # Write the second row
+    # Write the second row forwards
     csvwriter.writerow('Total Months: ' + str(total_months) + '\n')
     csvwriter.writerow('Total: $' + str(total_prof_loss) + '\n') 
     csvwriter.writerow('Average Change: $' + str(average_change) + '\n')
     csvwriter.writerow('Greatest Increase in Profits: $ ' + str(gt_revenue_increase) + gt_revenue_increase_month)
-    csvwriter.writerow('Greatest Decrease in Profits: $'  + str(gt_revenue_decrease) + gt_revenue_decrease_month)
+    csvwriter.writerow('Greatest Decrease in Profits: $' + str(gt_revenue_decrease) + gt_revenue_decrease_month)
     
     
 
--- python-challenge/PyBank/pybank_main2.py
+++ python-challenge/PyBank/pybank_main2.py
@@ -13,11 +13,47 @@ with open(file) as csvfile:
     month_list = []
     total_revenue = 0
     revenue_list = []
-
+    current_revenue_value = 0
+    previous_revenue_value = 0
+    calculated_revenue_list = []
     
 
     for row in csv_data:
-        month_count = month_count +1
-        month_list.append(str(rwo[0])
+
+        month_list.append(row[0])
+        month_count = len(month_list)
+        revenue_list.append(row[1])
+
+        for row in range(len(revenue_list)):
+            
+            if current_revenue_value != 0
+            print(row[0] + " is rated " + row[1] + " with a rating of " + row[5])
+
+            # BONUS: Set variable to confirm we have found the video
+            found = True
+
+            # BONUS: Stop at first results to avoid duplicates
+            break
+            current_revenue_value = revenue_list[row[0]]
+
+            #if revenue_list[row] >= gt_revenue_increase:
+            #gt_revenue_increase = prof_loss[row]
+            #gt_revenue_increase_month = months[row]
+            #print gt_revenue_increase_month
+
+            #elif prof_loss[row] < gt_revenue_decrease:
+            #gt_revenue_decrease = prof_loss[row]
+            #print gt_revenue_decrease        
+            #gt_revenue_decrease_month = months[row]
+            #print gt_revenue_decrease_month  
+
+        #total_prof_loss += prof_loss [row] 
+    #print total_prof_loss
+        #current_revenue_value = int(row[1])
     
-    
+        #calculated_revenue_value = current_revenue_value - previous_revenue_value
+        #calculated_revenue_list.append(calculated_revenue_value)
+
+        #average_change = (sum(revenue_list) / month_count) 
+        
+
