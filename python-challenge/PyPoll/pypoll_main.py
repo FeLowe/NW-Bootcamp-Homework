bbeb8d56ae5ce50fa779b26945f8e319df87a72a
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2019-03-10 20:24:23 -0500

    code to loops through each candidate name in candidates list and calculates candidate votes

--- python-challenge/PyPoll/pypoll_main.py
+++ python-challenge/PyPoll/pypoll_main.py
@@ -5,6 +5,27 @@ import csv
 # Set path for file
 file = os.path.join("election_data.csv")
 
+total_votes = 0
+candidates_list = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0  
+
 with open(file) as csvfile:
     csv_data = csv.reader(csvfile, delimiter=',')
     header_out = next(csv_data, None)
+
+#"Voter ID(row[0])	County(row[1])	Candidate"(row[2])
+    for row in csv_data:
+        #counts total votes
+        total_votes = total_votes +1
+
+       #gets candidate name at "Candidate" column
+        candidate_name = row[2]
+    #loops through all cadidate names in cadidates list and calculates candidate votes    
+    if candidate_name not in candidates_list:
+        candidates_list.append(candidate_name)
+        candidate_votes[candidate_name] = 0
+    candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
+        
+    print candidate_votes    
