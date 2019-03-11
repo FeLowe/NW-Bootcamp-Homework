94b6affa006006354a540a6ac6109d3bbae8270e
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2019-03-11 00:10:35 -0500

    switched casting of percentage variable from 'int' to 'float'

--- python-challenge/PyPoll/pypoll_main.py
+++ python-challenge/PyPoll/pypoll_main.py
@@ -8,8 +8,8 @@ file = os.path.join("election_data.csv")
 total_votes = 0
 candidates_list = []
 candidate_votes = {}
-winning_candidate = ""
-winning_count = 0  
+winner = ""
+winner_count = 0  
 
 with open(file) as csvfile:
     csv_data = csv.reader(csvfile, delimiter=',')
@@ -36,5 +36,19 @@ with open(file) as csvfile:
 
         #adds a vote to that candidate's count
         candidate_votes[candidate_name] = candidate_votes[candidate_name] + 1
-    #print candidate_votes
-    print total_votes
+    #print total_votes = 3521001
+    #print candidate_votes = {'Khan': 2218231, "O'Tooley": 105630, 'Correy': 704200, 'Li': 492940}
+
+    for candidate in candidate_votes:
+        #calculates percentage of each candidate votes
+        votes_per_candidate = candidate_votes.get(candidate)
+        #print votes_per_candidate = 2218231, 105630, 704200, 492940
+        vote_percentage_per_candidate = float(votes_per_candidate) / float(total_votes) * 100
+        print vote_percentage_per_candidate
+        #calculates winning vote count and candidate
+        if (total_votes > winner_count):
+            winner_count = total_votes
+            #print winner_count
+            winner_candidate = candidate
+        #print winner_count
+        #print winner_candidate    
