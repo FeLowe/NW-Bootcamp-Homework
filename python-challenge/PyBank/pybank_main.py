771d3524457fdaea0d5d036d7181966a07a82240
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2019-03-17 12:23:32 -0500

    worked on getting the number correctly

--- python-challenge/PyBank/pybank_main.py
+++ python-challenge/PyBank/pybank_main.py
@@ -6,7 +6,8 @@ import csv
 file = os.path.join("budget_data.csv")
 
 months = []
-prof_loss = []
+total_revenue = []
+total_revenue_sum = 0
 
 with open(file) as csvfile:
     csv_data = csv.reader(csvfile, delimiter=',')
@@ -16,36 +17,46 @@ with open(file) as csvfile:
         # push each month to months list
         months.append(row[0])
         # push each value to prof_loss
-        prof_loss.append(int(row[1]))
+        total_revenue.append(int(row[1]))
     #gets len of months, print single count number as it is oulside the loop. 
     total_months = len(months)
+    #print(total_revenue)
+    #sets variables to start at 0 value
+    gt_revenue_increase = total_revenue[0]
+    gt_revenue_decrease = total_revenue[0]
+    #print (gt_revenue_decrease)
+    #print (gt_revenue_decrease)
     
-    #set variables to start at 0 value
-    gt_revenue_increase = prof_loss[0]
-    gt_revenue_decrease = prof_loss[0]
-    total_prof_loss = 0
-    
-    #loop through each row in prof_loss list 
-    for row in range(len(prof_loss)):
+    #loops through each row in total_revenue list 
+    for row in range(0,len(total_revenue)):
 
         #gets greatest revenue increse value and month
-        if prof_loss[row] >= gt_revenue_increase:
-            gt_revenue_increase = prof_loss[row]
+        if total_revenue[row] >= gt_revenue_increase:
+            gt_revenue_increase = total_revenue[row]
+            #print(gt_revenue_increase)
             gt_revenue_increase_month = months[row]
         #gets greatest revenue descrease value and month
-        elif prof_loss[row] < gt_revenue_decrease:
-            gt_revenue_decrease = prof_loss[row]      
+        elif total_revenue[row] < gt_revenue_decrease:
+            gt_revenue_decrease = total_revenue[row]      
             gt_revenue_decrease_month = months[row]
         
-        #get total revenuw
- total_prof_loss += prof_loss [row] 
-    
+        #gets total revenue
+    total_revenue_sum = total_revenue_sum + total_revenue[row]
+    #print (total_revenue_sum)
+  
 #Average calculation       
-average_change = round(total_prof_loss / total_months,2) 
+average_change = round(sum(total_revenue) / len(months),2) 
 
+print(total_months)
+print(total_revenue_sum)
+print(average_change)
+print(gt_revenue_increase)
+print(gt_revenue_decrease)
+print(gt_revenue_increase_month)
+print(gt_revenue_decrease_month)
 #File to write results to
-utput_path = os.path.join("pybank_output.txt")
-output_path = os.path.join("pybank_output.csv")
+output_path = os.path.join("pybank_output.txt")
+#output_path = os.path.join("pybank_output.csv")
 
 
 # Open the file using "write" mode. Specify the variable to hold the contents
@@ -60,12 +71,12 @@ with open(output_path,'w') as csvfile:
     csvwriter.writerow('----------------------------' + '\n')
     # Write the second row forwards
     csvwriter.writerow('Total Months: ' + str(total_months) + '\n')
-    csvwriter.writerow('Total: $' + str(total_prof_loss) + '\n') 
+    csvwriter.writerow('Total: $' + str(total_revenue_sum) + '\n') 
     csvwriter.writerow('Average Change: $' + str(average_change) + '\n')
     csvwriter.writerow('Greatest Increase in Profits: $ ' + str(gt_revenue_increase) + gt_revenue_increase_month)
     csvwriter.writerow('Greatest Decrease in Profits: $' + str(gt_revenue_decrease) + gt_revenue_decrease_month)
     
-    
+
 
    
     
