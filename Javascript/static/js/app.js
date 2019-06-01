d9a9b986f88bed2cd4b6dac1485e5b39901b0b51
Fernanda Lowe”git config --global user.name Fernanda <fe_lowe@hotmail.com>
2019-06-01 18:42:54 -0500

    added code to render table data on webpage

--- Javascript/static/js/app.js
+++ Javascript/static/js/app.js
@@ -1,4 +1,15 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+data.forEach((ufo_sighting) => {
+  var row = tbody.append("tr");
+  Object.entries(ufo_sighting).forEach(([key, value]) => {
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
+
+
